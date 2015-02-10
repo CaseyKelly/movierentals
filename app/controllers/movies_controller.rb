2f7ab0aa12ccf96050709ff3f3b53d2c1f51492b
CaseyKelly <casey.kelly@colorado.edu>
2015-02-09 22:31:31 -0700

    add movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,13 @@
 class MoviesController < ApplicationController
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(params[:id])
+    if @movie.save
+      redirect_to new_movie_path
+    end
+  end
+
 end
